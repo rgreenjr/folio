e84b1b5efb568876e48f7b76eb3f79db6831b19b
Ron Green <rgreenjr@gmail.com>
2010-08-29 20:06:55 -0500

    remove unused webview delegate methods

--- Classes/WebViewController.rb
+++ Classes/WebViewController.rb
@@ -3,7 +3,7 @@ class WebViewController
   attr_accessor :webView, :item
   
   def awakeFromNib
-    @webView.resourceLoadDelegate = self
+    # @webView.resourceLoadDelegate = self
   end
   
   def item=(item)
@@ -11,11 +11,6 @@ class WebViewController
     url = NSURL.URLWithString(@item.uri)
     request = NSURLRequest.requestWithURL(url)
     @webView.mainFrame.loadRequest(request)
-  end
-  
-  # WebResourceLoadDelegate methods
-  
-  def webView(sender, resource:identifier, willSendRequest:request, redirectResponse:redirectResponse, fromDataSource:dataSource)
   end
   
 end
