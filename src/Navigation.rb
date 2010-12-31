7dd460f922ea219cdd205280c2316aaa23eaad1d
Ron Green <rgreenjr@gmail.com>
2010-12-31 15:29:12 -0600

    Check for dtb:PrimaryID as fall back when parsing ill-formed books.

--- src/Navigation.rb
+++ src/Navigation.rb
@@ -4,16 +4,16 @@ class Navigation
 
   def initialize(book)
     @hash  = {}
-    
     doc = REXML::Document.new(book.manifest.ncx.content)
-    
     @ncx_name = book.manifest.ncx.name
-        
     prefix = (doc.root.prefix != '') ? "#{doc.root.prefix}:" : ''
-
-    @id = doc.elements["/#{prefix}ncx/#{prefix}head/#{prefix}meta[@name='dtb:uid']"].attributes["content"]
+    uid = doc.elements["/#{prefix}ncx/#{prefix}head/#{prefix}meta[@name='dtb:uid']"]
+    # check for 'dtb:PrimaryID' as last resort (is this standard compliant?)
+    uid = doc.elements["/#{prefix}ncx/#{prefix}head/#{prefix}meta[@name='dtb:PrimaryID']"] unless uid
+    raise "Navigation id not found" unless uid
+    @id = uid.attributes["content"]
     @title = doc.elements["/#{prefix}ncx/#{prefix}docTitle/#{prefix}text"].text
-    
+
     if doc.elements["/ncx/docAuthor/text"]
       @docAuthor = doc.elements["/ncx/docAuthor/text"].text
     end
@@ -27,26 +27,28 @@ class Navigation
       parent = point_stack.shift
       element = xml_stack.shift
       element.elements.each_with_index("#{prefix}navPoint") do |e, i|
-        
+
         href, fragment = e.elements["#{prefix}content"].attributes["src"].split("#")
-        
+
         item = book.manifest.itemWithHref(href)
         raise "Navigation point reference not found: src=#{href}" unless item
-                
+
         childPoint           = Point.new
         childPoint.id        = e.attributes["id"]
+        
         childPoint.playOrder = e.attributes["playOrder"]
         childPoint.text      = e.elements["#{prefix}navLabel/#{prefix}text"].text
         childPoint.item      = item
         childPoint.fragment  = fragment
 
         insert(childPoint, -1, parent)
-        
+
         point_stack.insert(i, childPoint)
         xml_stack.insert(i, e)
       end
     end
-
+  rescue Exception => exception
+    Alert.runModal("Unable to open #{book.filepath} because an error occurred while parsing NCX.", exception.message)
   end
 
   def depth
@@ -77,30 +79,30 @@ class Navigation
       return point if index - idx == -1
     end
   end
-  
+
   def index_and_parent(point)
     each do |parent|
       index = parent.index(point)
       return index, parent if index
     end
   end
-  
+
   def insert(point, index, parent)
     raise "Navigation point ID already exists: id=#{point.id}" if @hash[id]      
     @hash[point.id] = point
     parent.insert(index, point)
   end
-  
+
   def move(point, index, parent)
     delete(point)
     insert(point, index, parent)
   end
-  
+
   def appendItem(item)
     point = Point.new(item, item.name)
     insert(point, -1, @root)
   end
-  
+
   def duplicate(point)
     index, parent = index_and_parent(point)
     clone = Point.new(point.item, point.text)
@@ -108,7 +110,7 @@ class Navigation
     @hash[clone.id] = clone
     clone
   end
-  
+
   def delete(point)
     index, parent = index_and_parent(point)
     parent.delete_at(index)
@@ -118,7 +120,7 @@ class Navigation
   def pointWithId(identifier)
     @hash[identifier]
   end
-  
+
   def save(directory)
     File.open("#{directory}/#{@ncx_name}", 'w') {|f| f.write(to_xml)}
   end
@@ -126,6 +128,15 @@ class Navigation
   def to_xml
     navigation = self
     ERB.new(Bundle.template("toc.ncx")).result(binding)
+  end
+
+  def to_s
+    buffer = "@navigation = {\n"
+    @hash.each do |id, point|
+      buffer << "  id=#{id} => href=#{point.item.href}\n"
+    end
+    buffer << "}"
+    buffer
   end
 
 end
