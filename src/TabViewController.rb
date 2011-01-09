fc408cb7595646576d0c6c6c1d93b174a3517699
Ron Green <rgreenjr@gmail.com>
2011-01-09 12:04:09 -0600

    Added editor splitview orientation UI to toolbar.

--- src/TabViewController.rb
+++ src/TabViewController.rb
@@ -1,6 +1,10 @@
 class TabViewController < NSViewController
+  
+  HORIZONTAL_ORIENTATION_TAG = 0
+  VERTICAL_ORIENTATION_TAG   = 1
 
   attr_accessor :splitView, :textViewController, :webViewController
+  attr_accessor :splitViewSegementedControl
 
   def awakeFromNib
     view.delegate = self
@@ -28,13 +32,22 @@ class TabViewController < NSViewController
   end
 
   def toggleSplitViewOrientation(sender)
-    if sender.title == 'Split Views Horizontally'
-      sender.title = 'Split Views Vertically'
-      @splitView.vertical = false
+    if sender == @splitViewSegementedControl
+      @splitViewSegementedControl.selectedSegment == HORIZONTAL_ORIENTATION_TAG ? makeSplitViewOrientationVertical : makeSplitViewOrientationHorizontal
     else
-      sender.title = 'Split Views Horizontally'
-      @splitView.vertical = true
+      @splitViewSegementedControl.selectedSegment == HORIZONTAL_ORIENTATION_TAG ? makeSplitViewOrientationHorizontal : makeSplitViewOrientationVertical
     end
+  end
+  
+  def makeSplitViewOrientationVertical
+    @splitViewSegementedControl.selectSegmentWithTag(HORIZONTAL_ORIENTATION_TAG)
+    @splitView.vertical = false
+    @splitView.adjustSubviews
+  end
+
+  def makeSplitViewOrientationHorizontal
+    @splitViewSegementedControl.selectSegmentWithTag(VERTICAL_ORIENTATION_TAG)
+    @splitView.vertical = true
     @splitView.adjustSubviews
   end
 
