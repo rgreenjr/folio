f8c8901b2a12158b79c6d3d91caf941c35f0ce85
Ron Green <rgreenjr@gmail.com>
2011-01-14 16:50:40 -0600

    Change ImageWell to use URLs so remote files can be dragged and dropped.

--- src/ImageWell.rb
+++ src/ImageWell.rb
@@ -1,18 +1,32 @@
 class ImageWell < NSImageView
 
-  attr_accessor :imagePath
+  attr_accessor :imageURL
 
   def performDragOperation(draggingInfo)
     dragSucceeded = super
     if dragSucceeded
-      filenamesXML = draggingInfo.draggingPasteboard.stringForType(NSFilenamesPboardType)
-      if filenamesXML
-        data = filenamesXML.dataUsingEncoding(NSUTF8StringEncoding)
-        filenames = NSPropertyListSerialization.propertyListFromData(data, mutabilityOption:NSPropertyListImmutable, format:nil, errorDescription:nil)
-        @imagePath = filenames.count >= 1 ? filenames[0] : nil
+      urlsXML = draggingInfo.draggingPasteboard.stringForType(NSURLPboardType)
+      if urlsXML
+        data = urlsXML.dataUsingEncoding(NSUTF8StringEncoding)
+        urls = NSPropertyListSerialization.propertyListFromData(data, mutabilityOption:NSPropertyListImmutable, format:nil, errorDescription:nil)
+        @imageURL = urls.empty? ? nil : NSURL.URLWithString(urls[0])
+        @imagePath = nil
       end
     end
     dragSucceeded
+  end
+
+  # TODO auto detect image type representation
+  def imagePath
+    return nil unless @imageURL
+    unless @imagePath
+      imageRep = image.representations[0]
+      data = imageRep.representationUsingType(NSJPEGFileType, properties:nil)
+      tmpDir = Dir.mktmpdir("folio-cover-image-")
+      @imagePath = File.join(tmpDir, @imageURL.path.lastPathComponent)
+      data.writeToFile(@imagePath, atomically:false)
+    end
+    @imagePath
   end
 
 end
