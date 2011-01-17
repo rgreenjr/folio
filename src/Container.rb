fa9cd1dae0754332142066b6da2400fd4a7a4f32
Ron Green <rgreenjr@gmail.com>
2011-01-17 17:30:46 -0600

    Change container string to constants; fix "Add File..." menu selector.

--- src/Container.rb
+++ src/Container.rb
@@ -1,29 +1,34 @@
 class Container
 
-  CONTAINER_XML_PATH = "/META-INF/container.xml"
+  META_INF_DIRECTORY           = "META-INF"
+  CONTAINER_XML_NAME           = "container.xml"
+  CONTAINER_XML_RELATIVE_PATH  = File.join(META_INF_DIRECTORY, CONTAINER_XML_NAME)
 
+  DEFAULT_CONTENT_OPF_NAME     = "content.opf"
+  DEFAULT_RELATIVE_PATH        = "OEBPS"
+  
   attr_reader :relativePath, :absolutePath, :opfDoc, :opfPath
 
   def initialize(unzipPath, book=nil)
     if book.nil?
-      @relativePath = 'OEBPS'
-      @absolutePath = @relativePath == '' ? unzipPath : File.join(unzipPath, @relativePath)
+      @relativePath = DEFAULT_RELATIVE_PATH
+      @absolutePath = makeAbsolutePath(unzipPath)
       FileUtils.mkdir_p(@absolutePath) unless File.exist?(@absolutePath)
       @opfPath = ''
       @opfDoc = nil
     else
       begin
-        xmlPath = File.join(unzipPath, CONTAINER_XML_PATH)
-        raise "The #{CONTAINER_XML_PATH} file is missing." unless File.exists?(xmlPath)
+        xmlPath = File.join(unzipPath, CONTAINER_XML_RELATIVE_PATH)
+        raise "The #{CONTAINER_XML_RELATIVE_PATH} file is missing." unless File.exists?(xmlPath)
 
         doc = REXML::Document.new(File.read(xmlPath))
 
         @opfPath = doc.root.elements["rootfiles/rootfile"].attributes["full-path"]
-        raise "The #{CONTAINER_XML_PATH} does not specify an OPF file." unless @opfPath
+        raise "The #{CONTAINER_XML_RELATIVE_PATH} does not specify an OPF file." unless @opfPath
 
         @relativePath = File.dirname(@opfPath).split('/').last
         @relativePath = '' if @relativePath == '.'
-        @absolutePath = @relativePath == '' ? unzipPath : File.join(unzipPath, @relativePath)
+        @absolutePath = makeAbsolutePath(unzipPath)
 
         @opfPath = File.join(unzipPath, @opfPath)
 
@@ -31,20 +36,26 @@ class Container
         @opfDoc = REXML::Document.new(File.read(@opfPath))
 
       rescue REXML::ParseException => exception
-        raise StandardError, "An error occurred while parsing #{CONTAINER_XML_PATH}: #{exception.explain}"
+        raise StandardError, "An error occurred while parsing #{CONTAINER_XML_RELATIVE_PATH}: #{exception.explain}"
       end
     end
   end
   
   def save(directory)
-    FileUtils.mkdir_p("#{directory}/#{@relativePath}")
-    FileUtils.mkdir_p("#{directory}/META-INF")
-    File.open("#{directory}#{CONTAINER_XML_PATH}", "w") {|f| f.write(to_xml)}
+    FileUtils.mkdir_p(File.join(directory, @relativePath))    
+    FileUtils.mkdir_p(File.join(directory, META_INF_DIRECTORY))
+    File.open(File.join(directory, CONTAINER_XML_RELATIVE_PATH), "w") {|f| f.write(to_xml)}
   end
 
   def to_xml
-    opfPath = @relativePath.empty? ? "content.opf" : "#{@relativePath}/content.opf"
-    ERB.new(Bundle.template("container.xml")).result(binding)
+    opfPath = @relativePath.empty? ? DEFAULT_CONTENT_OPF_NAME : File.join(@relativePath, DEFAULT_CONTENT_OPF_NAME)
+    ERB.new(Bundle.template(CONTAINER_XML_NAME)).result(binding)
+  end
+  
+  private
+  
+  def makeAbsolutePath(unzipPath)
+    @relativePath == '' ? unzipPath : File.join(unzipPath, @relativePath)
   end
 
 end
