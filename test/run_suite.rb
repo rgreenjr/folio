c6283e3e631d4fbc689b37fe55ea3f1283162637
Ron Green <rgreenjr@gmail.com>
2011-01-18 17:50:09 -0600

    Support run a specific unit test.

--- test/run_suite.rb
+++ test/run_suite.rb
@@ -8,4 +8,9 @@ require 'tempfile'
 require 'cgi'
 
 Dir.glob(File.expand_path('../../Classes/*.rb', __FILE__)).each { |klass| require klass }
-Dir.glob(File.expand_path('../**/*_test.rb', __FILE__)).each { |test| require test }
+
+if ARGV.empty?
+  Dir.glob(File.expand_path('../**/*_test.rb', __FILE__)).each { |test| require test }
+else
+  require File.expand_path("../#{ARGV[0]}", __FILE__)
+end
