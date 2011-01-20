146b6e8f9382c5ddcf2f20fec0575d50a6d7b826
Ron Green <rgreenjr@gmail.com>
2011-01-20 10:24:03 -0600

    Add font selection UI to preferences.

--- Classes/PreferencesController.rb
+++ Classes/PreferencesController.rb
@@ -1,16 +1,40 @@
 class PreferencesController < NSWindowController
-  
+
+  attr_accessor :fontTextField
+
   def init
     initWithWindowNibName("Preferences")
+    @editorFont = NSFont.systemFontOfSize(11.0)
+    self
+  end
+
+  def awakeFromNib
+    updateFontTextField
   end
 
   def showWindow(sender)
     super
     window.center
   end
-  
-  
+
   def showGeneralPane(sender)
+  end
+
+  def showFontPanel(sender)
+    fontManager = NSFontManager.sharedFontManager
+    fontManager.delegate = self
+    fontManager.orderFrontFontPanel(sender)
+  end
+
+  def changeFont(sender)
+    @editorFont = sender.convertFont(@editorFont)
+    updateFontTextField
+  end
+
+  private
+
+  def updateFontTextField
+    @fontTextField.stringValue = "#{@editorFont.displayName} #{@editorFont.pointSize.to_i} pt."
   end
 
 end
