817fd5ad51b79bdc3e42ce6e9007c66b6149e987
Ron Green <rgreenjr@gmail.com>
2011-12-21 16:28:01 -0600

    Add unit test stub for Item.

---
+++ test/Item_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+class ItemTest < Test::Unit::TestCase
+  
+  def setup
+    
+  end
+  
+  def test_title
+    # assert_equal("The Fall of the Roman Empire: A New History of Rome and the Barbarians", @book.metadata.title)
+  end
+  
+  def test_description
+    # assert_equal("In this groundbreaking book, Peter Heather proposes the theory that Rome generated its own nemesis.", @book.metadata.description)
+  end
+
+end
