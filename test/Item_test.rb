cc0cb0cfa996f04e81d22c7b55193417ff07fa70
Ron Green <rgreenjr@gmail.com>
2011-12-22 12:19:44 -0600

    Add instantiation unit tests for Item.

--- test/Item_test.rb
+++ test/Item_test.rb
@@ -3,15 +3,31 @@ require 'test/unit'
 class ItemTest < Test::Unit::TestCase
   
   def setup
-    
+    @root = Item.new(nil, "cover.jpg")
   end
   
-  def test_title
-    # assert_equal("The Fall of the Roman Empire: A New History of Rome and the Barbarians", @book.metadata.title)
+  def test_inititalize_root
+    assert_equal(nil, @root.parent)
+    assert_equal("cover.jpg", @root.name)
+    assert_not_nil(@root.id)
+    assert_equal(Media::JPG, @root.mediaType)
+    assert_equal(false, @root.expanded?)
+    assert_equal(0, @root.issues.size)
+    assert_equal(false, @root.directory?)
   end
-  
-  def test_description
-    # assert_equal("In this groundbreaking book, Peter Heather proposes the theory that Rome generated its own nemesis.", @book.metadata.description)
+
+  def test_inititalize_child
+    item = Item.new(@root, "cover.jpg")
+    @root << item
+    assert_equal(@root, item.parent)
+    assert_equal(true, item.hasParent?)
+    assert_equal("cover.jpg", item.name)
+    assert_not_nil(item.id)
+    assert_equal(Media::JPG, item.mediaType)
+    assert_equal(false, item.expanded?)
+    assert_equal(0, item.issues.size)
+    assert_equal(false, item.directory?)
+    assert_equal(1, item.parent.size)
   end
 
 end
