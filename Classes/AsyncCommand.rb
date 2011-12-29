97dcc3ca78d6049406ad8b81a4f2807b8ef7e3c1
Ron Green <rgreenjr@gmail.com>
2011-12-29 11:11:03 -0600

    Update thread name space string.

--- Classes/AsyncCommand.rb
+++ Classes/AsyncCommand.rb
@@ -2,7 +2,7 @@ class AsyncCommand
   def initialize(&block)
     # Each thread gets its own FIFO queue upon which we will dispatch
     # the delayed computation passed in the &block variable.
-    Thread.current[:futures] ||= Dispatch::Queue.new("com.mycompany.folio-async-#{Thread.current.object_id}")
+    Thread.current[:futures] ||= Dispatch::Queue.new("com.folioapp.async-#{Thread.current.object_id}")
     @group = Dispatch::Group.new 
     # Asynchronously dispatch the future to the thread-local queue.
     Thread.current[:futures].async(@group) { @value = block.call }
