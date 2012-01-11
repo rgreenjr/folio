4e8001858fe15470914b63b0eada42371959ce6f
Ron Green <rgreenjr@gmail.com>
2012-01-10 18:23:47 -0600

    Change TabView#selectTabCell to always re-assign selectedTab, even when already selected, to allow points with fragments to properly update.

--- Classes/TabView.rb
+++ Classes/TabView.rb
@@ -211,7 +211,6 @@ class TabView < NSView
   end
   
   def selectTabCell(tabCell, point=nil)
-    return if tabCell && tabCell == @selectedTabCell
     @selectedTabCell.selected = false if @selectedTabCell
     if tabCell
       tabCell.selected = true
