13e167b02c76f416cdc8d8767beacb302cfb4d0d
Ron Green <rgreenjr@gmail.com>
2012-01-13 18:31:05 -0600

    Return "0 B" in to_storage_size for values less than 0.

--- Classes/Numeric.rb
+++ Classes/Numeric.rb
@@ -1,8 +1,12 @@
 class Numeric
   def to_storage_size
-    units = [" B", " KB", " MB", " GB", " TB"]
-    e = (Math.log(self) / Math.log(1024)).floor
-    s = "%.2f" % (self.to_f / 1024**e)
-    s.sub(/\.?0*$/, units[e])
+    if self > 0
+      units = [" B", " KB", " MB", " GB", " TB"]
+      e = (Math.log(self) / Math.log(1024)).floor
+      s = "%.2f" % (self.to_f / 1024**e)
+      s.sub(/\.?0*$/, units[e])
+    else
+      "0 B"
+    end
   end
 end
