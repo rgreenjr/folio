904c3709f046f1a946d63a21ab466476574e95be
Ron Green <rgreenjr@gmail.com>
2012-01-18 23:37:20 -0600

    Parse item XML fragments asynchronously using a Grand Central Dispatch FIFO queue.

--- Classes/PointViewController.rb
+++ Classes/PointViewController.rb
@@ -1,14 +1,14 @@
 class PointViewController < NSViewController
-  
+
   FRAGMENT_POPUP_OFFSET = 2
-  
+
   attr_accessor :point
   attr_accessor :textField
   attr_accessor :idField
   attr_accessor :sourcePopup
   attr_accessor :fragmentComboBox
   attr_accessor :progressIndicator
-  
+
   def initWithBookController(controller)
     initWithNibName("PointView", bundle:nil)
     @bookController = controller
@@ -20,7 +20,7 @@ class PointViewController < NSViewController
     @point = point
     updateView(@point)
   end
-  
+
   def updatePoint(sender)
     if sender == @textField
       changeText(@point, sender.stringValue)
@@ -32,7 +32,7 @@ class PointViewController < NSViewController
       changeFragment(@point, @fragmentComboBox.stringValue)
     end
   end
-  
+
   def changeText(point, value)
     unless value.blank? || point.text == value
       undoManager.prepareWithInvocationTarget(self).changeText(point, point.text)
@@ -41,7 +41,7 @@ class PointViewController < NSViewController
     end
     updateView(point)
   end
-  
+
   def changeID(point, value)
     unless value.blank? || point.id == value
       if oldID = @bookController.document.navigation.changePointId(point, value)
@@ -53,7 +53,7 @@ class PointViewController < NSViewController
     end
     updateView(point)
   end
-  
+
   def changeSourceAndFragment(point, item, fragment)
     return if point.item == item
     undoManager.prepareWithInvocationTarget(self).changeSourceAndFragment(point, point.item, point.fragment)
@@ -63,7 +63,7 @@ class PointViewController < NSViewController
     @bookController.tabbedViewController.addObject(point)
     updateView(point)
   end
-  
+
   def changeFragment(point, fragment)
     return if point.fragment == fragment
     if fragment.blank? || point.item.hasFragment?(fragment)
@@ -75,35 +75,56 @@ class PointViewController < NSViewController
     end
     updateView(point)
   end
-  
+
   def comboBox(comboBox, objectValueForItemAtIndex:index)
     @point.item.fragments[index]
   end
-  
+
   def numberOfItemsInComboBox(comboBox)
-    return 0 unless @point || @fragmentQueue[@point]
-    if @point.item.fragmentsCached?
-      @fragmentComboBox.enabled = true
-      @fragmentQueue[point] = nil
-      @progressIndicator.hidden = true
-      @progressIndicator.stopAnimation(self)
-      return @point.item.fragments.size 
+    return 0 unless @point
+    item = @point.item
+    queueStatus = @fragmentQueue[item]
+    if queueStatus.nil?
+      if item.fragmentsCached?
+        enableFragmentComboBoxForItem(item)
+        return item.fragments.size 
+      else
+        puts "begin #{item.name}"
+        @fragmentQueue[item] = :loading
+        queue.async do
+          puts "loading #{item.name}"
+          if item.fragments
+            @fragmentQueue[item] = :complete
+          else
+            # returns nil on parsing failure
+            @fragmentQueue[item] = :error
+          end
+          @fragmentComboBox.noteNumberOfItemsChanged
+        end
+      end
+    elsif queueStatus == :loading
+      puts "continue #{item.name}"
+    elsif queueStatus == :complete
+      puts "complete #{item.name}\n-----------"
+      enableFragmentComboBoxForItem(item)
+      return item.fragments.size
+    elsif @fragmentQueue[item] == :error
+      puts "error #{item.name}"
+      enableFragmentComboBoxForItem(item)
     else
-      @fragmentQueue[@point] = point
-      self.performSelector('loadFragments:', withObject:@point, afterDelay:0.0)    
-      @progressIndicator.startAnimation(self)
-      @progressIndicator.hidden = false
-      @fragmentComboBox.enabled = false
-      return 0
+      puts "bad state #{item.name}"
+      enableFragmentComboBoxForItem(item)
     end
+    disableFragmentComboBox
+    return 0
   end
-  
+
   def comboBox(comboBox, completedString:uncompletedString)
     @point.item.closestFragment(uncompletedString)
   end  
-  
+
   private
-  
+
   def updateView(point)
     if point
       @textField.stringValue = point.text
@@ -114,7 +135,7 @@ class PointViewController < NSViewController
       @bookController.selectionViewController.reloadItem(point)
     end
   end
-  
+
   def updateSourcePopup(point)
     @sourcePopup.removeAllItems
     @items = []
@@ -127,12 +148,33 @@ class PointViewController < NSViewController
 
   def loadFragments(point)
     puts "loadFragments..."
-    point.item.fragments # force fragment parsing
+    if point.item.fragments
+      @fragmentQueue[point] = :complete
+    else
+      @fragmentQueue[point] = :error
+    end
     @fragmentComboBox.noteNumberOfItemsChanged
   end
-  
+
   def undoManager
     @undoManager ||= @bookController.window.undoManager
   end
-  
+
+  def enableFragmentComboBoxForItem(item)
+    @fragmentQueue[item] = nil
+    @progressIndicator.hidden = true
+    @progressIndicator.stopAnimation(self)
+    @fragmentComboBox.enabled = true
+  end
+
+  def disableFragmentComboBox
+    @progressIndicator.startAnimation(self)
+    @progressIndicator.hidden = false
+    @fragmentComboBox.enabled = false
+  end
+
+  def queue
+    @queue ||= Dispatch::Queue.new("com.folioapp.fragment-parsing-queue")
+  end
+
 end
