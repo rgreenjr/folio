b3f1467b811af19827c9c3fa0d17d93e10e7b6f9
Ron Green <rgreenjr@gmail.com>
2012-01-20 15:54:47 -0600

    Make textfield reject first responder status on right mouse click events.

--- Classes/MyOutlineView.rb
+++ Classes/MyOutlineView.rb
@@ -2,41 +2,41 @@ class MyOutlineView < NSOutlineView
 
   BLUE_TEXTURE_COLOR  = NSColor.colorWithPatternImage(NSImage.imageNamed("BlueTexture.png"))
   BLUE_TEXTURE_ENDCAP = NSImage.imageNamed("BlueTextureEndcap.png")
-
+  
   def awakeFromNib
     enclosingScrollView.drawsBackground = false
   end
-
+  
   def drawBackgroundImage
-    rect = enclosingScrollView.documentVisibleRect		
+    rect = enclosingScrollView.documentVisibleRect    
     BLUE_TEXTURE_COLOR.set
     NSRectFill(rect)
     endcapRect = NSMakeRect(rect.size.width - 2, rect.origin.y, 2, rect.size.height)
     BLUE_TEXTURE_ENDCAP.drawInRect(endcapRect, fromRect:NSZeroRect, operation:NSCompositeCopy, fraction:1.0)
   end
-
+  
   def drawBackgroundInClipRect(clipRect)
     super
     # must draw entire documentVisibleRect since clipping area has been 
     # set and not all the background will update properly.
     drawBackgroundImage
   end
-
+  
   def drawRect(drawRect)
     super
     drawBackgroundImage
   end
-
+  
   def expandItem(item, expandChildren:expandChildren)
     super
     needsDisplay = true
   end
-
+  
   def collapseItem(item, collapseChildren:collapseChildren)
     super
     needsDisplay = true
   end
-
+  
   def menuForEvent(event)
     if delegate
       window.makeFirstResponder(self)
@@ -50,12 +50,12 @@ class MyOutlineView < NSOutlineView
       super
     end
   end
-
-  def acceptsFirstResponder
-    event = NSApp.currentEvent
-    return true unless event
-    row = rowAtPoint(convertPoint(event.locationInWindow, fromView:nil))
-    isRowSelected(row) ? true : false
-  end
+  
+  # def acceptsFirstResponder
+  #   event = NSApp.currentEvent
+  #   return true unless event
+  #   row = rowAtPoint(convertPoint(event.locationInWindow, fromView:nil))
+  #   isRowSelected(row) ? true : false
+  # end
 
 end
--- Classes/MyTextField.rb
+++ Classes/MyTextField.rb
@@ -3,7 +3,10 @@ class MyTextField < NSTextField
   # Fix for Xcode bug that prevents NSTextFields from issuing 
   # text delegate method calls when they are inside a NSTeableCellView.
   def acceptsFirstResponder
-    delegate ? delegate.textShouldBeginEditing(self) : super
+    event = NSApp.currentEvent
+    return false if event && event.type == NSRightMouseDown
+    return delegate.textShouldBeginEditing(self) if delegate
+    super
   end
   
 end
