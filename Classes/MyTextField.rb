f4e3c98b17a54c8a2e24a033a20d250c915dbd68
Ron Green <rgreenjr@gmail.com>
2012-02-21 19:37:14 -0600

    Only call text delegate if delegate responds to method.

--- Classes/MyTextField.rb
+++ Classes/MyTextField.rb
@@ -1,13 +1,17 @@
 class MyTextField < NSTextField
 
   # Fix for Xcode bug that prevents NSTextFields from issuing 
-  # text delegate method calls when they are inside a NSTeableCellView.
+  # text delegate method calls when they are inside a NSTableCellView.
   # Also prevents NSTextFields from entering edit mode on right clicks.
   def acceptsFirstResponder
     event = NSApp.currentEvent
-    return false if event && event.type == NSRightMouseDown
-    return delegate.textShouldBeginEditing(self) if delegate
-    super
+    if event and event.type == NSRightMouseDown
+      false
+    elsif delegate and delegate.respond_to? "textShouldBeginEditing:"
+      delegate.textShouldBeginEditing(self)
+    else
+      super
+    end
   end
   
 end
